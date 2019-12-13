b3ac7f69753e72ee4f14734ca6727874b927e63c
sirchuckalot <charleypicker@yahoo.com>
2019-12-13 12:03:17 -0800

    Removed tabs and fixed indentations

--- av_bfm_memory.v
+++ av_bfm_memory.v
@@ -38,7 +38,7 @@ module av_bfm_memory
    input [burstw-1:0] av_burstcount_i,
    input              av_write_i,
    input              av_read_i,
-   
+
    output             av_waitrequest_o,
    output             av_readdatavalid_o,
    output [1:0]       av_response_o,
@@ -47,16 +47,16 @@ module av_bfm_memory
 `include "av_common.v"
 
    localparam bytes_per_dw = (dw/8);
-   localparam mem_words = (mem_size_bytes/bytes_per_dw);
+   localparam mem_words    = (mem_size_bytes/bytes_per_dw);
 
-   parameter ADR_LSB = $clog2(bytes_per_dw);
+   parameter ADR_LSB       = $clog2(bytes_per_dw);
 
    //Counters for read and write accesses
-   integer 		   reads  = 0;
-   integer 		   writes = 0;
+   integer reads  = 0;
+   integer writes = 0;
 
    // synthesis attribute ram_style of mem is block
-   reg [dw-1:0] 	mem [ 0 : mem_words-1 ]   /* verilator public */ /* synthesis ram_style = no_rw_check */;
+   reg [dw-1:0] mem [ 0 : mem_words-1 ]   /* verilator public */ /* synthesis ram_style = no_rw_check */;
 
    av_bfm_slave
      #(.aw (aw),
@@ -66,7 +66,7 @@ module av_bfm_memory
    bfm0
      (.av_clk_i           (av_clk_i),
       .av_rst_i           (av_rst_i),
-      
+
       .av_address_i       (av_address_i),
       .av_writedata_i     (av_writedata_i),
       .av_byteenable_i    (av_byteenable_i),
@@ -79,47 +79,47 @@ module av_bfm_memory
       .av_response_o      (av_response_o),
       .av_readdata_o      (av_readdata_o));
 
-   reg [aw-1:0] 	address;
-   reg [dw-1:0] 	data;
+   reg [aw-1:0] address;
+   reg [dw-1:0] data;
 
-   integer 		i;
-   integer 		delay;
-   integer 		seed;
+   integer      i;
+   integer      delay;
+   integer      seed;
 
    always begin
-      bfm0.init();
-      address = bfm0.address; //Fetch start address
+        bfm0.init();
+        address = bfm0.address; //Fetch start address
 
-      if(bfm0.op === WRITE)
-	writes = writes + 1;
-      else
-	reads = reads + 1;
-      while(bfm0.has_next) begin
-	 //Set error on out of range accesses
-	 if(address[31:ADR_LSB] > mem_words) begin
-	    $display("%0d : Error : Attempt to access %x, which is outside of memory", $time, address);
-	    bfm0.error_response();
-	 end else begin
-	    if(bfm0.op === WRITE) begin
-	       bfm0.write_ack(data);
-	       if(DEBUG) $display("%d : ram Write 0x%h = 0x%h %b", $time, address, data, bfm0.mask);
-	       for(i=0;i < dw/8; i=i+1)
-		 if(bfm0.mask[i])
-		   mem[address[31:ADR_LSB]][i*8+:8] = data[i*8+:8];
-	    end else begin
-	       data = {aw{1'b0}};
-	       for(i=0;i < dw/8; i=i+1)
-		 if(bfm0.mask[i])
-		   data[i*8+:8] = mem[address[31:ADR_LSB]][i*8+:8];
-	       if(DEBUG) $display("%d : ram Read  0x%h = 0x%h %b", $time, address, data, bfm0.mask);
-	       delay = $dist_uniform(seed, rd_min_delay, rd_max_delay);
-	       repeat(delay) @(posedge av_clk_i);
-	       bfm0.read_ack(data);
-	    end
-	 end
-	 if(bfm0.cycle_type === BURST_CYCLE)
-	   address = av_next_adr(address, CTI_INC_BURST, BTE_LINEAR, dw);
-      end
+        if(bfm0.op === WRITE)
+            writes = writes + 1;
+        else
+            reads = reads + 1;
+        while(bfm0.has_next) begin
+            //Set error on out of range accesses
+            if(address[31:ADR_LSB] > mem_words) begin
+                $display("%0d : Error : Attempt to access %x, which is outside of memory", $time, address);
+                bfm0.error_response();
+            end else begin
+                if(bfm0.op === WRITE) begin
+                    bfm0.write_ack(data);
+                    if(DEBUG) $display("%d : ram Write 0x%h = 0x%h %b", $time, address, data, bfm0.mask);
+                    for(i=0;i < dw/8; i=i+1)
+                        if(bfm0.mask[i])
+                        mem[address[31:ADR_LSB]][i*8+:8] = data[i*8+:8];
+                end else begin
+                    data = {aw{1'b0}};
+                    for(i=0;i < dw/8; i=i+1)
+                        if(bfm0.mask[i])
+                            data[i*8+:8] = mem[address[31:ADR_LSB]][i*8+:8];
+                    if(DEBUG) $display("%d : ram Read  0x%h = 0x%h %b", $time, address, data, bfm0.mask);
+                    delay = $dist_uniform(seed, rd_min_delay, rd_max_delay);
+                    repeat(delay) @(posedge av_clk_i);
+                    bfm0.read_ack(data);
+                end
+            end
+            if(bfm0.cycle_type === BURST_CYCLE)
+                address = av_next_adr(address, CTI_INC_BURST, BTE_LINEAR, dw);
+        end
    end
 
 endmodule // av_bfm_memory
--- av_bfm_slave.v
+++ av_bfm_slave.v
@@ -24,15 +24,15 @@ module av_bfm_slave
     parameter burstw = 8,
     parameter DEBUG = 0)
    (input               av_clk_i,
-    input 		        av_rst_i,
-    
-    input [aw-1:0] 	    av_address_i,
-    input [dw-1:0] 	    av_writedata_i,
+    input               av_rst_i,
+
+    input [aw-1:0]      av_address_i,
+    input [dw-1:0]      av_writedata_i,
     input [dw/8-1:0]    av_byteenable_i,
     input [burstw-1:0]  av_burstcount_i,
-    input 		        av_write_i,
-    input 		        av_read_i,
-    
+    input               av_write_i,
+    input               av_read_i,
+
     output reg          av_waitrequest_o,
     output reg          av_readdatavalid_o,
     output reg [1:0]    av_response_o,
@@ -40,11 +40,11 @@ module av_bfm_slave
 
 `include "av_common.v"
 
-   localparam Tp = 1;
+   localparam       Tp       = 1;
 
    reg              has_next = 1'b0;
 
-   reg              op = READ;
+   reg              op       = READ;
    reg [aw-1:0]     address;
    reg [dw-1:0]     data;
    reg [dw/8-1:0]   mask;
@@ -52,23 +52,23 @@ module av_bfm_slave
    reg [2:0]        burst_type;
    reg [burstw-1:0] count;
 
-   reg            err = 0;
-   
+   reg              err      = 0;
+
    wire av_cyc = av_write_i | av_read_i;
 
    task init;
         begin
-	        av_waitrequest_o <= #Tp 1'b1;
+            av_waitrequest_o <= #Tp 1'b1;
             av_readdatavalid_o <= #Tp 1'b0;
-	        av_readdata_o <= #Tp {dw{1'b0}};
-	        av_response_o <= #Tp RESPONSE_OKAY;
+            av_readdata_o <= #Tp {dw{1'b0}};
+            av_response_o <= #Tp RESPONSE_OKAY;
 
             if(av_rst_i !== 1'b0) begin
-	            if(DEBUG) $display("%0d : waiting for reset release", $time);
-	            @(negedge av_rst_i);
-	            @(posedge av_clk_i);
-	            if(DEBUG) $display("%0d : Reset was released", $time);
-	         end
+                if(DEBUG) $display("%0d : waiting for reset release", $time);
+                @(negedge av_rst_i);
+                @(posedge av_clk_i);
+                if(DEBUG) $display("%0d : Reset was released", $time);
+             end
 
             //Catch start of next cycle
             if (!av_cyc)
@@ -77,16 +77,16 @@ module av_bfm_slave
 
             //Make sure that av_cyc is still asserted at next clock edge to avoid glitches
             while(av_cyc !== 1'b1)
-	            @(posedge av_clk_i);
+                @(posedge av_clk_i);
             if(DEBUG) $display("%0d : Got av_cyc", $time);
 
             //cycle_type = get_cycle_type(av_cti_i);
             cycle_type = (|av_burstcount_i) ? BURST_CYCLE : CLASSIC_CYCLE;
 
-            op      = av_write_i ? WRITE : READ;            
-            address = av_address_i;
-            mask    = av_byteenable_i;
-            count   = av_burstcount_i;
+            op         = av_write_i ? WRITE : READ;
+            address    = av_address_i;
+            mask       = av_byteenable_i;
+            count      = av_burstcount_i;
 
             has_next = 1'b1;
         end
@@ -120,38 +120,38 @@ module av_bfm_slave
 
    task next;
         begin
-	        if(DEBUG) $display("%0d : next address=0x%h, data=0x%h, op=%b", $time, address, data, op);
+            if(DEBUG) $display("%0d : next address=0x%h, data=0x%h, op=%b", $time, address, data, op);
 
-	        av_readdata_o <= #Tp {dw{1'b0}};
-	        av_waitrequest_o <= #Tp 1'b1;
-            av_response_o <= #Tp RESPONSE_OKAY;
+            av_readdata_o    <= #Tp {dw{1'b0}};
+            av_waitrequest_o <= #Tp 1'b1;
+            av_response_o    <= #Tp RESPONSE_OKAY;
 
-	        if(err) begin
-	            if(DEBUG) $display("%0d, Error", $time);
-	            av_response_o <= #Tp RESPONSE_SLAVEERROR;
-	            has_next = 1'b0;
-	        end else begin
-	            if(op === READ) begin
-                    av_readdata_o <= #Tp data;
+            if(err) begin
+                if(DEBUG) $display("%0d, Error", $time);
+                av_response_o <= #Tp RESPONSE_SLAVEERROR;
+                has_next       = 1'b0;
+            end else begin
+                if(op === READ) begin
+                    av_readdata_o      <= #Tp data;
                     av_readdatavalid_o <= #Tp 1'b1;
-	            end
-	            av_waitrequest_o <= #Tp 1'b0;
-	        end
+                end
+                av_waitrequest_o <= #Tp 1'b0;
+            end
 
-	        @(posedge av_clk_i);
+            @(posedge av_clk_i);
 
-	        av_waitrequest_o <= #Tp 1'b1;
+            av_waitrequest_o <= #Tp 1'b1;
             av_readdatavalid_o <= #Tp 1'b0;
             av_response_o <= #Tp RESPONSE_OKAY;
 
-	        //has_next = !av_is_last(av_cti_i) & !err;
+            //has_next = !av_is_last(av_cti_i) & !err;
             has_next = (count > 1) & !err;
 
-	        if(op === WRITE) begin
-	            data = av_writedata_i;
-	            mask = av_byteenable_i;
-	        end
-	        address = av_address_i;
+            if(op === WRITE) begin
+                data = av_writedata_i;
+                mask = av_byteenable_i;
+            end
+            address = av_address_i;
             if (count > 0)
                 count = count - 1;
         end
